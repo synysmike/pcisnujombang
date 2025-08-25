8c014f086e644e336667631b4bcd9dc577c05e03
synysmike <sembuarang@yahoo.com>
2025-08-25 12:47:35 +0000

    feat: Update hero section to use video background and enhance layout

--- application/views/public/home/hero.php
+++ application/views/public/home/hero.php
@@ -1,7 +1,69 @@
-<div class="th-hero-wrapper hero-1" id="hero">
-	<div class="swiper th-slider hero-slider1" id="heroSlide1" data-slider-options='{"effect":"fade", "autoHeight": "true"}'>
-		<div class="swiper-wrapper" id="swiperWrapper">
-			<!-- Dynamic slides will be appended here -->
-		</div>
+<!--======== Hero Section ========-->
 
+<style>
+	.video-bg-wrapper {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		z-index: -1;
+	}
+
+	.video-bg-wrapper iframe {
+		width: 100vw;
+		height: 100vh;
+		pointer-events: none;
+	}
+</style>
+
+
+<div class="th-hero-wrapper hero-3" id="hero">
+	<div class="video-bg-wrapper">
+		<iframe
+			src="https://www.youtube.com/embed/DjduqOyDKik?autoplay=1&mute=1&controls=0&loop=1&playlist=DjduqOyDKik&modestbranding=1&showinfo=0"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen></iframe>
 	</div>
+
+	<!-- <div class="shape-mockup hero-shape-3-1 d-lg-block d-none" data-top="20%" data-left="50%">
+		<div class="color-masking shake">
+			<div class="masking-src" data-mask-src="assets/img/hero/hero-bg-shape2-3.png"></div>
+			<img src="assets/img/hero/hero-bg-shape2-3.png" alt="shape">
+		</div>
+	</div>
+	<div class="shape-mockup hero-shape-3-2 jump" data-top="25%" data-left="5%">
+		<div class="color-masking">
+			<div class="masking-src" data-mask-src="assets/img/hero/hero-bg-shape2-1.png"></div>
+			<img src="assets/img/hero/hero-bg-shape2-1.png" alt="shape">
+		</div>
+	</div>
+	<div class="shape-mockup hero-shape-3-3 jump" data-bottom="0" data-left="-2%">
+		<div class="color-masking2">
+			<div class="masking-src" data-mask-src="assets/img/shape/hand-shape3.png"></div>
+			<img src="assets/img/shape/hand-shape3.png" alt="shape">
+		</div>
+	</div> -->
+	<div class="hero-bg-3-1" data-bg-src="https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg" data-mask-src="https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg"></div>
+	<div class="container">
+		<div class="row gx-40 align-items-center">
+			<div class="col-lg-6">
+				<div class="hero-style3">
+					<!-- <span class="sub-title after-none">Give Hope For Homeless</span> -->
+					<!-- <h1 class="hero-title">
+						<span class="title1">Donate For A</span>
+						<span class="title2">Better <span class="text-theme2 d-inline-block">World</span></span>
+					</h1> -->
+					<!-- <p class="hero-text">Explore the variety of volunteer opportunities available. From event planning and fundraising to fieldwork and administrative support</p> -->
+					<!-- <div class="btn-wrap">
+						<a href="about.html" class="th-btn">Discover Now<i class="fa-solid fa-arrow-up-right ms-2"></i></a>
+						<a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" class="play-btn style3 popup-video"><i class="fas fa-play"></i></a>
+					</div> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--======== / Hero Section ========--><!--==============================
